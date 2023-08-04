793a477939f47d091207821d0ce5e58e592fd957
garfield-kim <dolgodolah@gmail.com>
2023-08-04 22:36:02 +0900

    Create Ameria Server
    - reactor-netty 기반의 서버에서 armeria 기반의 서버로 변경하여 실행

--- src/main/kotlin/com/example/armeria/ArmeriaApplication.kt
+++ src/main/kotlin/com/example/armeria/ArmeriaApplication.kt
@@ -1,11 +1,22 @@
 package com.example.armeria
 
+import com.linecorp.armeria.common.HttpResponse
+import com.linecorp.armeria.server.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class ArmeriaApplication
 
 fun main(args: Array<String>) {
-	runApplication<ArmeriaApplication>(*args)
+	val server = newServer(8080)
+	server.closeOnJvmShutdown()
+	server.start().join()
+}
+
+fun newServer(port: Int): Server {
+	val builder = Server.builder()
+	return builder
+		.http(port)
+		.service("/") { ctx, req -> HttpResponse.of("Hello World")}
+		.build()
 }
