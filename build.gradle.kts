ec31ea1038f7cec0604f39d24e3539a21546b005
garfield-kim <dolgodolah@gmail.com>
2023-08-11 13:34:08 +0900

    gRPC 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
 	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.2"
+	id("com.google.protobuf") version "0.8.19"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 }
@@ -18,10 +20,35 @@ repositories {
 	mavenCentral()
 }
 
+protobuf {
+	// Configure the protoc executable.
+	protoc {
+		// Download from the repository.
+		artifact = "com.google.protobuf:protoc:3.22.3"
+	}
+
+	// Locate the codegen plugins.
+	plugins {
+		// Locate a plugin with name 'grpc'.
+		id("grpc") {
+			// Download from the repository.
+			artifact = "io.grpc:protoc-gen-grpc-java:1.56.0"
+		}
+	}
+	generateProtoTasks {
+		ofSourceSet("main").forEach {
+			it.plugins {
+				id("grpc") {}
+			}
+		}
+	}
+}
+
 dependencies {
 	listOf(
 		"armeria-spring-boot3-webflux-starter",
-		"armeria-kotlin"
+		"armeria-kotlin",
+		"armeria-grpc"
 	).forEach {
 		implementation("com.linecorp.armeria:${it}:1.24.3")
 	}
@@ -33,6 +60,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("javax.annotation:javax.annotation-api:1.3.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
