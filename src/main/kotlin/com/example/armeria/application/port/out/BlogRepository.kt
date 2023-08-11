edb3a96095b7648ee7c24f13900a19683b651370
garfield-kim <dolgodolah@gmail.com>
2023-08-11 11:37:32 +0900

    REST SERVICE 디렉토리 변경

---
+++ src/main/kotlin/com/example/armeria/application/port/out/BlogRepository.kt
@@ -0,0 +1,9 @@
+//package com.example.armeria.`rest-api-annotated-service`.application.port.out
+//
+//import com.example.armeria.`rest-api-annotated-service`.application.domain.BlogPost
+//import reactor.core.publisher.Mono
+//
+//interface BlogRepository {
+//
+//    fun findById(id: Long): Mono<BlogPost>
+//}
